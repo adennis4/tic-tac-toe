51681e5e672f4a04ef3e5ece4bef64158050eba7
Andrew Dennis <adennis4@gmail.com>
2012-05-01 13:10:36 -0500

    adds moves to game

--- app/models/game_board.rb
+++ app/models/game_board.rb
@@ -5,4 +5,19 @@ class GameBoard < ActiveRecord::Base
   def start_game
     self.current_state = Array.new(9)
   end
+  
+  def makes_move(mark, posn)
+    if current_state[posn] == nil
+      current_state[posn] = mark
+    else
+      @message = "This spot was already taken. Your probably should pick a new one"
+    end
+  end
+  
+  def winner
+    winning_combos = [ [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                       [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                       [0, 4, 8], [2, 4, 6] ]
+
+  end
 end
--- spec/models/game_board_spec.rb
+++ spec/models/game_board_spec.rb
@@ -1,18 +1,42 @@
 require 'spec_helper'
 
 describe GameBoard do
+  before do
+    @game1 = GameBoard.create
+    @game1.start_game
+  end
   
   context 'game_start' do
     it 'has 9 positions' do
-      game1 = GameBoard.create
-      game1.start_game
-      game1.current_state.count.should eq(9)
+      @game1.current_state.count.should eq(9)
     end
   
     it 'starting positions have no values' do
-      game1 = GameBoard.create
-      game1.start_game
-      game1.current_state.compact.should be_empty
+      @game1.current_state.compact.should be_empty
     end
+  end
+  
+  context 'makes_move' do
+    it 'marks a given position' do
+      @game1.makes_move("X", 1)
+      @game1.current_state[1].should == "X"
+    end
+    
+    it 'raises an error for a position that is already taken' do
+      good_move = @game1.makes_move("X", 4)
+      bad_move  = @game1.makes_move("X", 4)
+      
+      bad_move.should be_instance_of(String)
+    end
+  end
+  
+  context 'win' do
+    it 'recognizes a win for three consecutive marks' do
+      @game1.makes_move("X", 0)
+      @game1.makes_move("X", 3)
+      @game1.makes_move("X", 6)
+      @game1.game_result.should be_true
+    end
+
   end
 end
